1d8079d999cf97d1bcd4f5d6ec8c90aae47c319f
Sukhrobjon Saidov <00016004@wiut.uz>
2024-12-05 16:31:28 +0500

    The first testing commits

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WAD._00016004.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Searching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Searching", "Testing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
